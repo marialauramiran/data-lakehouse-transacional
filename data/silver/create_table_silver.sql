38ec585ac535ef91007c6458db6a2dc4dee48bff
Maria Laura <57109043+marialauramiran@users.noreply.github.com>
2026-04-10 23:51:04 -0300

    Update create_table_silver.sql

--- data/silver/create_table_silver.sql
+++ data/silver/create_table_silver.sql
@@ -1,34 +1,34 @@
 CREATE OR REPLACE TABLE silver.silver_credit_card_fraud (
 
-    transaction_id INT COMMENT 'Identificador único da operação (renomeado de index_op)',
+    index_op INT COMMENT 'Identificador único da operação (renomeado de index_op)',
 
-    transaction_timestamp TIMESTAMP COMMENT 'Data e hora da transação padronizada',
+    trans_date_trans_time TIMESTAMP COMMENT 'Data e hora da transação padronizada',
 
-    merchant_name STRING COMMENT 'Nome do estabelecimento',
+    merchant STRING COMMENT 'Nome do estabelecimento',
 
-    merchant_category STRING COMMENT 'Categoria do estabelecimento',
+    category STRING COMMENT 'Categoria do estabelecimento',
 
-    transaction_amount DECIMAL(18,2) COMMENT 'Valor da transação',
+    amt DECIMAL(18,2) COMMENT 'Valor da transação',
 
     city STRING COMMENT 'Cidade da transação normalizada',
 
     state STRING COMMENT 'Estado da transação (UF padronizada)',
 
-    customer_latitude DOUBLE COMMENT 'Latitude do cliente convertida para numérico',
+    latitude DOUBLE COMMENT 'Latitude do cliente convertida para numérico',
 
-    customer_longitude DOUBLE COMMENT 'Longitude do cliente convertida para numérico',
+    longitude DOUBLE COMMENT 'Longitude do cliente convertida para numérico',
 
-    city_population INT COMMENT 'População da cidade',
+    city_pop INT COMMENT 'População da cidade',
 
-    customer_job STRING COMMENT 'Profissão do cliente padronizada',
+    job STRING COMMENT 'Profissão do cliente padronizada',
 
     dt_birthday DATE COMMENT 'Data de nascimento do cliente (convertida de timestamp)',
 
-    transaction_number STRING COMMENT 'Identificador único da transação',
+    trans_num STRING COMMENT 'Identificador único da transação',
 
-    merchant_latitude DOUBLE COMMENT 'Latitude do estabelecimento',
+    merch_lat DOUBLE COMMENT 'Latitude do estabelecimento',
 
-    merchant_longitude DOUBLE COMMENT 'Longitude do estabelecimento',
+    merch_long DOUBLE COMMENT 'Longitude do estabelecimento',
 
     is_fraud INT COMMENT 'Indicador de fraude (1 = fraude, 0 = não fraude)'
 
